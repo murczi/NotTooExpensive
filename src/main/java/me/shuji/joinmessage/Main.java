983cd2d5c3c0be2e0981a6197125a5496f368ceb
Shuji <marcellalter@gmail.com>
2024-07-05 19:02:53 +0200

    format and gitignore
    Signed-off-by: Shuji <marcellalter@gmail.com>

--- src/main/java/me/shuji/joinmessage/Main.java
+++ src/main/java/me/shuji/joinmessage/Main.java
@@ -15,46 +15,46 @@ import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin implements Listener {
 
-  FileConfiguration config = getConfig();
-  Logger console = getLogger();
+	FileConfiguration config = getConfig();
+	Logger console = getLogger();
 
-  @Override
-  public void onEnable() {
-    setConfig();
+	@Override
+	public void onEnable() {
+		setConfig();
 
-    if (config.getBoolean("enabled")) {
-      onDisable();
-    }
+		if (config.getBoolean("enabled")) {
+			onDisable();
+		}
 
-    getServer().getPluginManager().registerEvents(this, this);
-  }
+		getServer().getPluginManager().registerEvents(this, this);
+	}
 
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent e) {
-    Player player = e.getPlayer();
-    e.joinMessage(Component.text(this.trans(String.format(config.getString("joinMessage").replace("{player}", player.getName())))));
-    player.sendMessage(this.trans(String.format(config.getString("messageToPlayer").replace("{player}", player.getName()))));
-  }
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		e.joinMessage(Component.text(this.trans(String.format(config.getString("joinMessage").replace("{player}", player.getName())))));
+		player.sendMessage(this.trans(String.format(config.getString("messageToPlayer").replace("{player}", player.getName()))));
+	}
 
-  @EventHandler
-  public void onPlayerLeave(PlayerQuitEvent e) {
-    Player player = e.getPlayer();
-    e.quitMessage(Component.text(this.trans(String.format(config.getString("quitMessage").replace("{player}", player.getName())))));
-    console.info(player.getName() + " logged off at " + " X: " + player.getLocation().x() + " Y: " + player.getLocation().y() + " Z: " + player.getLocation().z());
-  }
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent e) {
+		Player player = e.getPlayer();
+		e.quitMessage(Component.text(this.trans(String.format(config.getString("quitMessage").replace("{player}", player.getName())))));
+		console.info(player.getName() + " logged off at " + " X: " + player.getLocation().x() + " Y: " + player.getLocation().y() + " Z: " + player.getLocation().z());
+	}
 
-  @SuppressWarnings("deprecation")
-  private String trans(String arg0) {
-    return ChatColor.translateAlternateColorCodes('&', arg0);
-  }
+	@SuppressWarnings("deprecation")
+	private String trans(String arg0) {
+		return ChatColor.translateAlternateColorCodes('&', arg0);
+	}
 
-  private void setConfig() {
-    config.addDefault("enabled", true);
-    config.addDefault("messageToPlayer", "Hello {player}");
-    config.addDefault("joinMessage", "{player} joined. o/");
-    config.addDefault("quitMessage", "{player} left. :c");
-    config.options().copyDefaults(true);
-    saveConfig();
-  }
+	private void setConfig() {
+		config.addDefault("enabled", true);
+		config.addDefault("messageToPlayer", "Hello {player}");
+		config.addDefault("joinMessage", "{player} joined. o/");
+		config.addDefault("quitMessage", "{player} left. :c");
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
 
 }
